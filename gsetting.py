4752bf648106d51b585f4c881feb0fac4c331774
Jiří Stránský <jistr@jistr.com>
2018-12-17 12:56:33 +0100

    Adopt new module conventions
    Trying to use the module with newest Ansible resulted in this error:
    
    "New-style module did not handle its own exit"
    
    Adopt new module conventions (call to module.exit_json) to fix this
    issue.

--- gsetting.py
+++ gsetting.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-import json
 import re
 import subprocess
 
@@ -102,11 +101,11 @@ def main():
     if changed and not module.check_mode:
         _set_value(schemadir, user, key, value)
 
-    print(json.dumps({
+    module.exit_json(**{
         'changed': changed,
         'key': key,
         'value': value,
         'old_value': old_value,
-    }))
+    })
 
 main()
